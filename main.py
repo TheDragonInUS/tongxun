24c0e6219b7959110f1771aa146be88c191876f6
lion with  rabbit <1820245610@qq.com>
2018-10-20 13:51:58 +0800

    v 1.03 抽象成类使用

--- main.py
+++ main.py
@@ -6,40 +6,41 @@ import socket
 import threading
 
 
-def borad_(udp):
-    udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    udp.sendto('Client broadcast message!\n'.encode(
-        'utf-8'), ('<broadcast>', 1314))
+class UdpChat(object):
+    def __init__(self):
+        self.udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp.bind(("", 1314))
+
+    def broadcast(self):
+        self.udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.udp.sendto('Client broadcast message!\n'.encode(
+            'utf-8'), ('<broadcast>', 1314))
+
+    def send_mas(self, ip_port):
+        while True:
+            massage = input("\n请输入您要的信息：\n")
+            self.udp.sendto(massage.encode(), ip_port)
+
+    def receive_mas(self):
+        while True:
+            massage = self.udp.recvfrom(2048)
+            if massage[1][0] == "192.168.43.43":
+                print("\n广播发送成功\n")
+            else:
+                print("接收发的消息是", massage[0].decode())
+
+    def run(self):
+
+        self.broadcast()
+        send_mas_1 = threading.Thread(
+            target=self.send_mas, kwargs={
+                "ip_port": (
+                    "192.168.43.233", 1314)})
+        receive_mas_2 = threading.Thread(target=self.receive_mas)
+
+        send_mas_1.start()
+        receive_mas_2.start()
 
 
-def send_mas(udp, ip_port):
-    while True:
-        massage = input("\n请输入您要的信息：\n")
-        udp.sendto(massage.encode(), ip_port)
-
-
-def reci_mas(udp):
-    while True:
-        massage = udp.recvfrom(2048)
-        if massage[1][0] == "192.168.43.43":
-            print("\n广播发送成功\n")
-        else:
-            print("接收发的消息是", massage[0].decode())
-
-
-def run():
-
-    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp.bind(("", 1314))
-    borad_(udp=udp)
-
-    send_mas_1 = threading.Thread(target=send_mas, kwargs={"udp": udp, "ip_port": ("192.168.43.233", 1314)})
-    reci_mas_2 = threading.Thread(target=reci_mas, kwargs={"udp": udp})
-
-    send_mas_1.start()
-    reci_mas_2.start()
-
-
-run()
-
-
+a = UdpChat()
+a.run()
